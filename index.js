d737a9a14079b0c47635ae36fa9edaf65b53a242
msv-karthik <msvkarthik12@gmail.com>
2024-11-15 22:44:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let button = document.getElementById('submit');
+
+button.addEventListener('click', () => {
+    const height = parseInt(document.getElementById('height').value);
+    const weight = parseInt(document.getElementById('weight').value);
+    const result = document.getElementById('output');
+    let height_status=false, weight_status=false;
+
+    if(height === '' || isNaN(height) || (height <= 0)){
+        document.getElementById('output').innerHTML = 'Please provide a valid height';
+    }else{
+        document.getElementById('output').innerHTML = '';
+        height_status=true;
+    }
+
+    if(weight === '' || isNaN(weight) || (weight <= 0)){
+        document.getElementById('output').innerHTML = 'Please provide a valid weight';
+    }else{
+        document.getElementById('output').innerHTML = '';
+        weight_status=true;
+    }
+
+    if(height_status && weight_status){
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+        if(bmi < 18.6){
+            result.innerHTML = 'Under weight : ' + bmi;
+        }else if(bmi >= 18.6 && bmi < 24.9){
+            result.innerHTML = 'Normal : ' + bmi;
+        }else{
+            result.innerHTML = 'Over weight : ' + bmi;
+        }
+    }else{
+        alert('The form has errors');
+        result.innerHTML = '';
+    }
+});
